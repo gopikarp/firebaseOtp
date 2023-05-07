5ef4a22f5a0ad1a4f02c11f6f3490ff17cf4c906
gopika <gokurprabhu@gmail.com>
2023-05-07 20:45:14 +0530

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,19 @@
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/material.dart';
+import 'package:phone_otp_ui/phone.dart';
+import 'package:phone_otp_ui/verify.dart';
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
+  runApp(
+    MaterialApp(
+      initialRoute: 'phone',
+      debugShowCheckedModeBanner: false,
+      routes: {
+        'phone': (context) => MyPhone(),
+        'verify': (context) => MyVerify()
+      },
+    ),
+  );
+}
